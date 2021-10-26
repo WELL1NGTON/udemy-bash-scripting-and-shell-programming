33149525cfd83a45eddab96277b5a60527a33187
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-10-26 12:19:18 -0300

    feat: section 10 exercises
    Done exercises from section 10.

---
+++ Section10/PraticeExercises/ex02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Exercise 2:
+#
+# Modify the previous exercise so that script continues, even if an error
+# occurs. This time all three ls commands will execute.
+
+ls $HOME
+ls $HOME/AAAAAAAAAAAAAAAAAAAAAAAAAAAA
+ls $HOME/.local
